615ceb361c68c972c8ec8f9ea2a7c58e10eaf465
Adam Mocarski <adammocarski22@gmail.com>
2022-09-28 19:17:54 +0200

    Add generatorID

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const randomId =(idLength) => {
+    let id =''
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    const charsAmount =characters.length;
+
+    for(let i = 0; i < idLength; i++){
+        id += characters.charAt(Math.floor(Math.random() * charsAmount));
+    }
+    return id;
+
+}
+
+module.exports = randomId;
